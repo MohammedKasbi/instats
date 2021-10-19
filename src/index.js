64ff591dd9c21b5c67c0ec24ebad5ae9f025e109
MohammedKasbi <mohammed.kasbi45@gmail.com>
2021-10-19 17:51:51 +0200

    base-model

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './components/App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.render (
   <React.StrictMode>
     <App />
   </React.StrictMode>,
