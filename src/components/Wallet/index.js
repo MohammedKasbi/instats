9e0767f8ea3a1c1f5b360c334985df32ee110153
MohammedKasbi <mohammed.kasbi45@gmail.com>
2021-10-26 10:18:08 +0200

    feat: doughnut --DOING

--- src/components/Wallet/index.js
+++ src/components/Wallet/index.js
@@ -1,9 +1,44 @@
+import { Doughnut  } from 'react-chartjs-2';
 import './style.scss';
 
-const Wallet = () => (
-  <div className="wallet">
-    Portefeuille
-  </div>
-);
+const Wallet = () => {
+  const doughnutData = {
+    labels: ['FIN888', 'EmpireX', 'Gold Robot', 'TTD', 'UAG Trade', 'Whalinvest'],
+    datasets: [{
+      data: [5347.76, 5974.09, 2106.80, 542.56, 489.36, 371.71],
+      backgroundColor: [
+        'rgba(252, 145, 65, 1)',
+        'rgba(61, 255, 249, 1)',
+        'rgba(240, 255, 109, 1)',
+        'rgba(186, 66, 255, 1)',
+        'rgba(254, 67, 144, 1)',
+        'rgba(66, 147, 254, 1)',
+      ],
+      borderColor: '#282c34',
+      borderWidth: 2,
+      hoverOffset: 4,
+      cutout: '90%',
+    }],
+  }
+
+  const doughnutOptions = {
+    maintainAspectRatio: false,
+    legend: {
+      display: true,
+      position: 'bottom',
+    },
+  }
+
+  return (
+    <div className="wallet">
+      <div className="wallet__pie">
+        <Doughnut data={doughnutData} options={doughnutOptions} />
+
+      </div>
+      <div className="wallet__pie-accounts"></div>
+      <div className="wallet__accounts-list"></div>
+    </div>
+  );
+};
 
 export default Wallet;
